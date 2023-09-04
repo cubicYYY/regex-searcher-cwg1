487602c85cd172ae56aa85facf76f5c4686d8766
yyy <yyy2015c01@gmail.com>
2023-09-04 23:49:32 +0800

    Modulized

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
+pub mod regex_searcher;
+
 use clap::{App, Arg};
 use colored::*;
-use regex::Regex;
-use std::path::Path;
+use regex_searcher::find;
 use std::process;
-use std::{fs, path::PathBuf};
-use tracing::{info, instrument};
+use tracing::info;
 use tracing_subscriber::FmtSubscriber;
 
-type PathVec = Vec<PathBuf>;
-type RegexVec = Vec<Regex>;
-
 fn main() {
+    // Init info tracker (from tracing)
     let subscriber = FmtSubscriber::builder().finish();
     tracing::subscriber::set_global_default(subscriber).expect("Failed to set tracing subscriber");
 
+    // Set all args
+    // I should use a declarative way...
     let matches = App::new("Regex")
         .arg(
             Arg::new("paths")
@@ -43,11 +43,6 @@ fn main() {
         )
         .get_matches();
 
-    // Pre-checks
-    let mut paths: PathVec =
-        Vec::with_capacity(matches.get_many::<String>("paths").unwrap().count());
-    let mut regexes: RegexVec =
-        Vec::with_capacity(matches.get_many::<String>("regexes").unwrap().count());
     info!(
         "{} 条路径",
         matches.get_many::<String>("paths").unwrap().count()
@@ -57,28 +52,12 @@ fn main() {
         matches.get_many::<String>("regexes").unwrap().count()
     );
 
-    if let Some(path_strs) = matches.get_many::<String>("paths") {
-        for path in path_strs {
-            if !is_valid_path(path.as_str()) {
-                eprintln!("无效的路径'{}'", path);
-                process::exit(1);
-            }
-            paths.push(PathBuf::from(path));
-        }
-    }
-
-    if let Some(regex_strs) = matches.get_many::<String>("regexes") {
-        for regex in regex_strs {
-            if !is_valid_pattern(regex) {
-                eprintln!("无效的正则表达式'{}'", regex);
-                process::exit(1);
-            }
-            regexes.push(Regex::new(regex).unwrap());
-        }
-    }
-
-    // Ready to search
-    match find(&paths, &regexes, matches.is_present("verbose")) {
+    // Ready to search!
+    match find(
+        &matches.get_many::<String>("paths").unwrap().collect(),
+        &matches.get_many::<String>("regexes").unwrap().collect(),
+        matches.is_present("verbose"),
+    ) {
         Ok(matches) => {
             println!("{}", "=".repeat(30));
             if matches.is_empty() {
@@ -96,64 +75,4 @@ fn main() {
             process::exit(1);
         }
     }
-}
-
-fn is_valid_path(path_str: &str) -> bool {
-    Path::new(path_str).exists()
-}
-
-fn is_valid_pattern(pattern: &str) -> bool {
-    match Regex::new(pattern) {
-        Ok(_) => true,
-        Err(_) => false,
-    }
-}
-
-#[instrument] // to log internally
-fn find(
-    paths: &PathVec,
-    regexes: &RegexVec,
-    is_verbose: bool,
-) -> Result<Vec<String>, Box<dyn std::error::Error>> {
-    info!("Search starting...");
-    let mut matches = Vec::new();
-    for path in paths {
-        walk_tree(path.as_ref(), regexes, &mut matches, 0, is_verbose)?;
-    }
-    Ok(matches)
-}
-
-#[instrument]
-fn walk_tree(
-    current: &Path,
-    regexes: &RegexVec,
-    matches: &mut Vec<String>,
-    level: usize,
-    is_verbose: bool,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let get_full_name: fn(&Path) -> String = |x: &Path| x.to_string_lossy().to_string();
-    if current.is_dir() {
-        if is_verbose {
-            print!("|{}", String::from("--").repeat(level));
-            println!("搜寻目录: {}", get_full_name(&current));
-        }
-        for entry in fs::read_dir(current)? {
-            let path = entry?.path();
-            walk_tree(&path, regexes, matches, level + 1, is_verbose)?;
-        }
-    } else if let Some(filename) = current.file_name().and_then(|s| s.to_str()) {
-        for regex in regexes {
-            if regex.is_match(filename) {
-                if is_verbose {
-                    print!("|{}", String::from("--").repeat(level));
-                    println!("{}", format!("[*] 匹配成功: {}", filename).green());
-                }
-                matches.push(get_full_name(&current));
-            } else {
-                print!("|{}", String::from("--").repeat(level));
-                println!("{}", format!("[-] 检查: {}", filename));
-            }
-        }
-    }
-    Ok(())
 }
---
+++ src/regex_searcher.rs
@@ -0,0 +1,88 @@
+type PathVec = Vec<PathBuf>;
+type RegexVec = Vec<Regex>;
+use colored::*;
+use regex::Regex;
+use std;
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::process;
+use tracing::{info, instrument};
+
+fn is_valid_path(path_str: &str) -> bool {
+    Path::new(path_str).exists()
+}
+
+fn is_valid_pattern(pattern: &str) -> bool {
+    match Regex::new(pattern) {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+
+#[instrument] // to log internally
+pub fn find(
+    path_strs: &Vec<&String>,
+    regex_strs: &Vec<&String>,
+    is_verbose: bool,
+) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    // Pre-checks
+    let mut paths: PathVec = Vec::with_capacity(path_strs.len());
+    let mut regexes: RegexVec = Vec::with_capacity(regex_strs.len());
+
+    for path in path_strs {
+        if !is_valid_path(path.as_str()) {
+            eprintln!("无效的路径'{}'", path);
+            process::exit(1);
+        }
+        paths.push(PathBuf::from(path));
+    }
+
+    for regex in regex_strs {
+        if !is_valid_pattern(regex) {
+            eprintln!("无效的正则表达式'{}'", regex);
+            process::exit(1);
+        }
+        regexes.push(Regex::new(regex).unwrap());
+    }
+    info!("Search starting...");
+    let mut matches = Vec::new();
+    for path in paths {
+        walk_tree(path.as_ref(), regexes.as_ref(), &mut matches, 0, is_verbose)?;
+    }
+    Ok(matches)
+}
+
+#[instrument]
+fn walk_tree(
+    current: &Path,
+    regexes: &RegexVec,
+    matches: &mut Vec<String>,
+    level: usize,
+    is_verbose: bool,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let get_full_name: fn(&Path) -> String = |x: &Path| x.to_string_lossy().to_string();
+    if current.is_dir() {
+        if is_verbose {
+            print!("|{}", String::from("--").repeat(level));
+            println!("搜寻目录: {}", get_full_name(&current));
+        }
+        for entry in fs::read_dir(current)? {
+            let path = entry?.path();
+            walk_tree(&path, regexes, matches, level + 1, is_verbose)?;
+        }
+    } else if let Some(filename) = current.file_name().and_then(|s| s.to_str()) {
+        for regex in regexes {
+            if regex.is_match(filename) {
+                if is_verbose {
+                    print!("|{}", String::from("--").repeat(level));
+                    println!("{}", format!("[*] 匹配成功: {}", filename).green());
+                }
+                matches.push(get_full_name(&current));
+            } else {
+                print!("|{}", String::from("--").repeat(level));
+                println!("{}", format!("[-] 检查: {}", filename));
+            }
+        }
+    }
+    Ok(())
+}
